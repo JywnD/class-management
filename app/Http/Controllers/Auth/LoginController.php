0d04c55c1f57f604b566e30a43ee162c97f8016d
JywnD <dang.ht203794@sis.hust.edu.vn>
2023-02-20 17:23:04 +0700

    fix redirect route when logined

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    // protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     protected function authenticated(Request $request, $user)
     {
